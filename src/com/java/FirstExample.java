6ab61d8635dd5ed8ea9bf754dd8ed774a0e44aae
Egorka1812 <egor.polugrudovo@gmail.com>
2022-02-12 13:12:45 -0600

    hello there

--- src/com/java/FirstExample.java
+++ src/com/java/FirstExample.java
@@ -3,7 +3,11 @@ package com.java;
 public class FirstExample {
 
 	public static void main(String[] args) {
+ 
+		
+		System.out.println("Test");
+		System.out.println("Hello there");
+	
 
-	}
-
+     }
 }
